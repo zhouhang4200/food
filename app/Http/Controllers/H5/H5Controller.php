bc3822077e8572487670f2f2089b8643fe6d7949
zhouhang <zhouhang4200@163.com>
2019-06-14 15:10:16 +0800

    静默授权

--- app/Http/Controllers/H5/H5Controller.php
+++ app/Http/Controllers/H5/H5Controller.php
@@ -11,7 +11,7 @@ class H5Controller extends Controller
 {
     function index()
     {
-//        H5Controller::getCode();
+        H5Controller::getCode();
 
         return view('vue');
     }
@@ -19,24 +19,29 @@ class H5Controller extends Controller
     public static function getCode()
     {
         try {
-            $config = [
-                'app_id' => config('pay.wechat.app_id'),
-                'scope'  => 'snsapi_base',
-                'oauth'  => [
-                    'scopes'        => ['snsapi_base'],
-                    'response_type' => 'code',
-                    'callback'      => config('app.h5_domain') . '/h5/callback',
-                ],
-            ];
-            myLog('code_config', ['data' => $config]);
-            $app   = new Application($config);
+            // 静默授权，跳转获取 code
+            $url = sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base#wechat_redirect",
+                config('pay.wechat.app_id'), urlencode(config('app.h5_domain') . '/h5/callback'));
+            return redirect($url);
 
-            $oauth = $app->oauth->redirect()->send();
-            myLog('code_app', ['data' => $app->oauth]);
-
-            myLog('code_response', ['data' => $oauth]);
-
-            return $oauth;
+//            $config = [
+//                'app_id' => config('pay.wechat.app_id'),
+//                'scope'  => 'snsapi_base',
+//                'oauth'  => [
+//                    'scopes'        => ['snsapi_base'],
+//                    'response_type' => 'code',
+//                    'callback'      => config('app.h5_domain') . '/h5/callback',
+//                ],
+//            ];
+//            myLog('code_config', ['data' => $config]);
+//            $app   = new Application($config);
+//
+//            $oauth = $app->oauth->redirect()->send();
+//            myLog('code_app', ['data' => $app->oauth]);
+//
+//            myLog('code_response', ['data' => $oauth]);
+//
+//            return $oauth;
         } catch (\Exception $e) {
             myLog('code_error', ['data' => '[' . $e->getLine() . ']' . $e->getMessage()]);
         }
