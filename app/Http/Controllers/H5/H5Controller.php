21eba11d86f390146959d86d74f73841ab952765
zhouhang <zhouhang4200@163.com>
2019-06-14 16:20:16 +0800

    判断是支付宝还是微信登录

--- app/Http/Controllers/H5/H5Controller.php
+++ app/Http/Controllers/H5/H5Controller.php
@@ -13,40 +13,48 @@ class H5Controller extends Controller
     function index(Request $request)
     {
         try {
-            if (! session('app_id')) {
-                $code = $request->input('code', ''); // 获取微信授权的code
+            //判断是不是微信
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+                if (! session('app_id')) {
+                    $code = $request->input('code', ''); // 获取微信授权的code
 
-                if ($code) {
-                    myLog('code_response', ['code' => $code]);
+                    if ($code) {
+                        myLog('code_response', ['code' => $code]);
 
-                    $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.config('pay.wechat.app_id').'&secret='.config('pay.wechat.secret').'&code='.$code.'&grant_type=authorization_code';
-                    $client = new Client();
+                        $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.config('pay.wechat.app_id').'&secret='.config('pay.wechat.secret').'&code='.$code.'&grant_type=authorization_code';
+                        $client = new Client();
 
-                    $response = $client->request('get', $url, [
-                        'multipart' => [],
-                    ]);
+                        $response = $client->request('get', $url, [
+                            'multipart' => [],
+                        ]);
 
-                    $result = $response->getBody()->getContents();
+                        $result = $response->getBody()->getContents();
 
-                    $result = json_decode($result, true);
+                        $result = json_decode($result, true);
 
-                    myLog('user_response', ['user' => $result]);
+                        myLog('user_response', ['user' => $result]);
 
-                    if (isset($result['openid']) && $result['openid']) {
-                        session('open_id', $result['openid']);
-                        myLog('openid_response', ['user' => $result['openid']]);
+                        if (isset($result['openid']) && $result['openid']) {
+                            session('open_id', $result['openid']);
+                            myLog('openid_response', ['openid' => $result['openid']]);
 
-                        return view('vue');
+                            return view('vue');
+                        } else {
+                            throw new \Exception('openid 获取失败！');
+                        }
                     } else {
-                        throw new \Exception('openid 获取失败！');
+                        // 静默授权，跳转获取 code
+                        $url = sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base#wechat_redirect",
+                            config('pay.wechat.app_id'), urlencode('http://'.config('app.h5_domain') . '/h5/callback'));
+                        Header("Location: $url");
                     }
                 } else {
-                    // 静默授权，跳转获取 code
-                    $url = sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base#wechat_redirect",
-                        config('pay.wechat.app_id'), urlencode('http://'.config('app.h5_domain') . '/h5/callback'));
-                    Header("Location: $url");
+                    return view('vue');
                 }
-            } else {
+            }
+
+            //判断是不是支付宝
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'AlipayClient') !== false) {
                 return view('vue');
             }
         } catch (\Exception $e) {
