b375204b87927308ba57ce8b0c6eff47c263f29a
zhouhang <zhouhang4200@163.com>
2019-06-15 14:09:04 +0800

    增加日志

--- app/Http/Controllers/H5/H5Controller.php
+++ app/Http/Controllers/H5/H5Controller.php
@@ -34,44 +34,19 @@ class H5Controller extends Controller
             if ($vue == 'order') {
                 if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) { //判断是不是微信
                     myLog('second-wechat', ['query' => $query, 'cookie' => session('open_id'), 'vue' => $vue]);
+
                     if (! session('open_id')) { // 不存在openid
-                        myLog('three-no-openid', ['query' => $query, 'cookie' => session('open_id'), 'vue' => $vue]);
-//                        $code = $request->input('code', ''); // 获取微信授权的code
-//
-//                        if ($code) {
-//                            myLog('code_response', ['code' => $code]);
-//
-//                            $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.config('pay.wechat.app_id').'&secret='.config('pay.wechat.secret').'&code='.$code.'&grant_type=authorization_code';
-//                            $client = new Client();
-//
-//                            $response = $client->request('post', $url, [
-//                                'multipart' => [],
-//                            ]);
-//
-//                            $result = $response->getBody()->getContents();
-//
-//                            $result = json_decode($result, true);
-//
-//                            myLog('user_response', ['user' => $result]);
-//
-//                            if (isset($result['openid']) && $result['openid']) {
-//                                session()->put('open_id', $result['openid']);
-//                                myLog('openid_response', ['openid' => $result['openid'], 'session' => session('open_id')]);
-//
-//                                return view('vue');
-//                            } else {
-//                                throw new \Exception('openid 获取失败！');
-//                            }
-//                        } else {
-                            // 静默授权，跳转获取 code
-                            $original_url = 'http://'.config('app.h5_domain').'/h5/'.$vue.'?'.$query;
-                            $url = sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base#wechat_redirect",
-                                config('pay.wechat.app_id'), urlencode('http://'.config('app.h5_domain') . '/auth/wechat/code/callback?original_url='.$original_url));
+                        // 静默授权，跳转获取 code
+                        $original_url = 'http://'.config('app.h5_domain').'/h5/'.$vue.'?'.$query;
 
-                            Header("Location: $url");
+                        myLog('three-no-openid', ['original_url' => $original_url, 'cookie' => session('open_id'), 'vue' => $vue]);
 
-                            exit();
-//                        }
+                        $url = sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base#wechat_redirect",
+                            config('pay.wechat.app_id'), urlencode('http://'.config('app.h5_domain') . '/auth/wechat/code/callback?original_url='.$original_url));
+
+                        Header("Location: $url");
+
+                        exit();
                     } else { // 存在oepnid 则让通行
                         myLog('three-have-openid', ['query' => $query, 'cookie' => session('open_id'), 'vue' => $vue]);
                         return view('vue');
