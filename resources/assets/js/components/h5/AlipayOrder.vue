146a8f18327dc54bb76fda1333665a35b83ed0b4
zhouhang <zhouhang4200@163.com>
2019-06-17 11:50:49 +0800

    测试

--- resources/assets/js/components/h5/AlipayOrder.vue
+++ resources/assets/js/components/h5/AlipayOrder.vue
@@ -204,13 +204,13 @@
                     let query = this.$route.query;
                     let jsApiParameters = '';
                     this.$api.h5Pay({amount:amount, detail:detail, open_id:open_id, merchant_id:merchant_id, seat_id:seat_id, table_id:table_id, query:query}).then(res => {
-                        this.$message({
-                            type: 'info',
-                            message: document.getElementByTagName("body")[0].innerHTML
-                        });
+                        // this.$message({
+                        //     type: 'info',
+                        //     message: document.getElementsByTagName('body')[0].innerHTML.append(res.pay_form)
+                        // });
                         // alert(data.pay_form);
                         if (res.status === 1) {
-                            document.getElementByTagName("body")[0].innerHTML.append(res.pay_form);
+                            document.getElementsByTagName("body")[0].innerHTML.append(res.pay_form);
                         } else {
                             this.$message({
                                 type: 'info',
