2de58f3868203c84de86b434436dab9aa9a410e5
zhouhang <zhouhang1807@stnts.com>
2020-05-11 15:39:09 +0800

    优化代码

--- app/Http/Requests/Category.php
+++ app/Http/Requests/Category.php
@@ -4,18 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Category extends FormRequest
+class Category extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
--- app/Http/Requests/User.php
+++ app/Http/Requests/User.php
@@ -8,18 +8,8 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 
-class User extends FormRequest
+class User extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -29,7 +19,7 @@ class User extends FormRequest
     {
         return [
             'phone' => 'required|numeric',
-            'password' => 'required|between:8,16',
+            'password' => 'required',
         ];
     }
 
@@ -39,14 +29,6 @@ class User extends FormRequest
             'phone.required' => '手机号必填',
             'phone.numeric' => '手机号必须为数字',
             'password.required' => '密码必填',
-            'password.between' => '密码必须在8-16位之间',
         ];
-    }
-
-    protected function failedValidation(Validator $validator)
-    {
-        $error = $validator->errors()->first();
-
-        throw new HttpResponseException(response()->json(['status' => 0, 'content' => $error, 'data' => '']), Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
