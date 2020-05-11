4d2c8b5ddb4daf4e4f52830a954fe7c690c7dc70
zhouhang <zhouhang1807@stnts.com>
2020-05-11 11:47:03 +0800

    初始化

---
+++ app/Console/Commands/Generate.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class Generate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:project';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '初始化，添加管理员什么的';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            User::create([
+                'name' => '周航',
+                'phone' => '13437284998',
+                'password' => bcrypt('admin888'),
+                'date' => Carbon::now()->toDateString(),
+            ]);
+
+            $this->info('初始化成功');
+        } catch (\Exception $e) {
+            $this->error('初始化失败：'.$e->getMessage());
+        }
+    }
+}
---
+++ app/Http/Repositories/H5Repository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories;
+
+class H5Repository extends BaseRepository
+{
+
+}
